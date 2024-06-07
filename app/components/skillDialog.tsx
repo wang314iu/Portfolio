396803b54916bb3b03f043b4ada8be5beb7e11db
Jun Wang <alicek@Juns-Mac-mini.fios-router.home>
2024-06-06 22:39:26 -0400

    fix skill section

--- app/components/skillDialog.tsx
+++ app/components/skillDialog.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import { TransitionProps } from "@mui/material/transitions";
 import SkillRating from "./skillRating";
 import { rates } from "../constants";
-import {
-  IconButton,
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  Slide,
-} from "@mui/material";
-// import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, Slide, Drawer } from "@mui/material";
 import { IoMdClose } from "react-icons/io";
 
 const Transition = React.forwardRef(function Transition(
@@ -31,43 +23,32 @@ interface Props {
 
 const SkillDialog = ({ open, handleClose, content, title }: Props) => {
   return (
-    <Dialog
-      open={open}
-      TransitionComponent={Transition}
-      keepMounted
-      onClose={handleClose}
-      aria-describedby="alert-dialog-slide-description"
-      className="h-full w-[90%] md:w-[60%] lg:w-[40%] bg-black text-white"
-    >
-      <DialogTitle className="bg-black text-white pr-8 pt-4">
-        {title}
-      </DialogTitle>
-      <IconButton
-        aria-label="close"
-        onClick={handleClose}
-        sx={{
-          position: "absolute",
-          right: 0,
-          top: -5,
-          color: (theme) => theme.palette.grey[400],
-        }}
-      >
-        <IoMdClose />
-      </IconButton>
-      <DialogContent className="bg-black text-white">
-        <DialogContentText className="text-inherit">
-          {open && (
-            <>
-              {content.map((skill) => (
-                <div key={skill} className="mb-3">
-                  {skill} <SkillRating rating={rates[skill]} />
-                </div>
-              ))}
-            </>
-          )}
-        </DialogContentText>
-      </DialogContent>
-    </Dialog>
+    <Drawer open={open} onClose={handleClose}>
+      <div className="min-w-[300px] md:min-w-[350px] h-full py-8 px-6 bg-black text-white">
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            color: (theme) => theme.palette.grey[400],
+            position: "absolute",
+            right: 8,
+            top: 8,
+          }}
+        >
+          <IoMdClose />
+        </IconButton>
+        <h4 className="text-xl font-bold mt-8 mb-6"> {title}</h4>
+        {open && (
+          <>
+            {content.map((skill) => (
+              <div key={skill} className="mb-3">
+                {skill} <SkillRating rating={rates[skill]} />
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    </Drawer>
   );
 };
 
